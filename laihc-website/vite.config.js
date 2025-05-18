58ea3e68268774016ac915833eb52525c9773526
Mike Roguez <mike.roguez@gmail.com>
2025-05-17 21:41:03 -0600

    build(Deployment): :rocket: Configura para poder realizar deploy a GitHub Pages

--- laihc-website/vite.config.js
+++ laihc-website/vite.config.js
@@ -6,6 +6,7 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/laihc/', // 👈 necesario para GitHub Pages
   plugins: [
     vue(),
     vueDevTools(),
